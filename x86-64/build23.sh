93bb3111bfe39d1daf943c531ec75a34ffd74b1c
BrewSips <49049987+BrewSips@users.noreply.github.com>
2025-10-03 22:14:08 +0800

    Update build23.sh

--- x86-64/build23.sh
+++ x86-64/build23.sh
@@ -52,11 +52,11 @@ PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 # ======== shell/custom-packages.sh =======
@@ -74,12 +74,13 @@ if echo "$PACKAGES" | grep -q "luci-app-openclash"; then
     echo "✅ 已选择 luci-app-openclash，添加 openclash core"
     mkdir -p files/etc/openclash/core
     # Download clash_meta
-    META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+    META_URL="https://github.com/vernesong/OpenClash/raw/refs/heads/core/master/smart/clash-linux-amd64-v2.tar.gz"
     wget -qO- $META_URL | tar xOvz > files/etc/openclash/core/clash_meta
     chmod +x files/etc/openclash/core/clash_meta
-    # Download GeoIP and GeoSite
-    wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O files/etc/openclash/GeoIP.dat
-    wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O files/etc/openclash/GeoSite.dat
+    # Download Country GeoSite and Model
+    wget -q https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/lite/Country.mmdb -O files/etc/openclash/Country.mmdb
+    wget -q https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat -O files/etc/openclash/GeoSite.dat
+    wget -q https://github.com/vernesong/mihomo/releases/download/LightGBM-Model/Model.bin -O files/etc/openclash/Model.bin
 else
     echo "⚪️ 未选择 luci-app-openclash"
 fi
